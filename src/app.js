b83748ba078b338c261237af7758b2491ced4e91
Sergey Polyakov <psergv72@outlook.com>
2026-08-01 22:09:49 +0300

    feat: refresh alarms on config sync and app start

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import { BaseApp } from '@zeppos/zml/base-app'
 import { log as Logger } from '@zos/utils'
+import { refreshAlarms } from './utils/schedule'
+import { ZML_METHODS } from './utils/constants'
 
 const logger = Logger.getLogger('aibolit-app')
 
@@ -8,6 +10,13 @@ App(
     globalData: {},
     onCreate() {
       logger.log('app onCreate invoked')
+      refreshAlarms()
+    },
+    onCall(data) {
+      logger.log(`app onCall method: ${data && data.method}`)
+      if (data && data.method === ZML_METHODS.CONFIG_SYNCED) {
+        refreshAlarms()
+      }
     },
     onDestroy() {
       logger.log('app onDestroy invoked')
